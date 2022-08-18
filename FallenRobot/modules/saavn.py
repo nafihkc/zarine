d6b6523bda47248be493e438627258b7381c4c6c
NAFIH KC (CAPTAIN) <103903674+nafihkc@users.noreply.github.com>
2022-08-18 15:02:03 +0530

    Create saavn.py

---
+++ FallenRobot/modules/saavn.py
@@ -0,0 +1,51 @@
+import os
+from pyrogram import filters
+import wget
+import requests
+from FallenRobot import pbot as Mbot
+from asgiref.sync import sync_to_async
+from requests import get
+def get_arg(message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+@sync_to_async
+def thumb_down(album_id,img):
+    with open(f"/tmp/thumbnails/{album_id}.jpg","wb") as file:
+        file.write(get(img).content)
+    return f"/tmp/thumbnails/{album_id}.jpg"
+
+@Mbot.on_message(filters.command('saavn') & filters.text)
+async def song(client, message):
+    message.chat.id
+    message.from_user["id"]
+    try:
+       args = message.text.split(None, 1)[1]
+    except:
+        return await message.reply("/saavn requires an argument.")
+    if args.startswith(" "):
+        await message.reply("/saavn requires an argument.")
+        return ""
+    pak = await message.reply('Downloading...')
+    try:
+#Ahh The Fuck is too long😒
+        r = requests.get(f"https://jostapi.herokuapp.com/saavn?query={args}")
+    except Exception as e:
+        await pak.edit(str(e))
+        return
+    sname = r.json()[0]["song"]
+    slink = r.json()[0]["media_url"]
+    ssingers = r.json()[0]["primary_artists"]
+    album_id = r.json()[0]["albumid"]
+    img = r.json()[0]["image"]
+    thumbnail = wget.download(img)
+    file = wget.download(slink)
+    ffile = file.replace("mp4", "m4a")
+    os.rename(file, ffile)
+    await pak.edit('Uploading...')
+    await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"{sname} - from saavn",thumb=thumbnail)
+    os.remove(ffile)
+    await pak.delete()
