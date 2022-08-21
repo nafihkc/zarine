ca44725851153cbb353810ab9d81c419df97a33c
NAFIH KC (CAPTAIN) <103903674+nafihkc@users.noreply.github.com>
2022-08-21 12:30:08 +0530

    Update rules_sql.py

--- FallenRobot/modules/sql/rules_sql.py
+++ FallenRobot/modules/sql/rules_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from ShuKurenaiXRoBot.modules.sql import BASE, SESSION
+from FallenRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Column, String, UnicodeText, distinct, func
 
 
