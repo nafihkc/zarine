0bff4f72e0fc435014b8a12ff0a53f618cede208
NAFIH KC (CAPTAIN) <103903674+nafihkc@users.noreply.github.com>
2022-08-22 06:19:50 +0530

    Update rules.py

--- FallenRobot/modules/rules.py
+++ FallenRobot/modules/rules.py
@@ -13,10 +13,11 @@ from telegram import (
     User,
 )
 from telegram.error import BadRequest
-from telegram.ext import CallbackContext, CommandHandler, Filters
+from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import escape_markdown
 
 
+@run_async
 def get_rules(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     send_rules(update, chat_id)
@@ -26,7 +27,6 @@ def get_rules(update: Update, context: CallbackContext):
 def send_rules(update, chat_id, from_pm=False):
     bot = dispatcher.bot
     user = update.effective_user  # type: Optional[User]
-    reply_msg = update.message.reply_to_message
     try:
         chat = bot.get_chat(chat_id)
     except BadRequest as excp:
@@ -37,37 +37,21 @@ def send_rules(update, chat_id, from_pm=False):
                 "fix this.\nMaybe they forgot the hyphen in ID",
             )
             return
-        raise
+        else:
+            raise
 
     rules = sql.get_rules(chat_id)
     text = f"The rules for *{escape_markdown(chat.title)}* are:\n\n{rules}"
 
     if from_pm and rules:
         bot.send_message(
-            user.id,
-            text,
-            parse_mode=ParseMode.MARKDOWN,
-            disable_web_page_preview=True,
+            user.id, text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
         )
     elif from_pm:
         bot.send_message(
             user.id,
             "The group admins haven't set any rules for this chat yet. "
             "This probably doesn't mean it's lawless though...!",
-        )
-    elif rules and reply_msg:
-        reply_msg.reply_text(
-            "Please click the button below to see the rules.",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="Rules",
-                            url=f"t.me/{bot.username}?start={chat_id}",
-                        ),
-                    ],
-                ],
-            ),
         )
     elif rules:
         update.effective_message.reply_text(
@@ -76,20 +60,20 @@ def send_rules(update, chat_id, from_pm=False):
                 [
                     [
                         InlineKeyboardButton(
-                            text="Rules",
-                            url=f"t.me/{bot.username}?start={chat_id}",
-                        ),
-                    ],
-                ],
+                            text="Rules", url=f"t.me/{bot.username}?start={chat_id}"
+                        )
+                    ]
+                ]
             ),
         )
     else:
         update.effective_message.reply_text(
             "The group admins haven't set any rules for this chat yet. "
-            "This probably doesn't mean it's lawless though...!",
+            "This probably doesn't mean it's lawless though...!"
         )
 
 
+@run_async
 @user_admin
 def set_rules(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
@@ -100,18 +84,14 @@ def set_rules(update: Update, context: CallbackContext):
         txt = args[1]
         offset = len(txt) - len(raw_text)  # set correct offset relative to command
         markdown_rules = markdown_parser(
-            txt,
-            entities=msg.parse_entities(),
-            offset=offset,
+            txt, entities=msg.parse_entities(), offset=offset
         )
 
         sql.set_rules(chat_id, markdown_rules)
         update.effective_message.reply_text("Successfully set rules for this group.")
-    else:
-        update.effective_message.reply_text("There's... no rules?")
 
 
-@connection_status
+@run_async
 @user_admin
 def clear_rules(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
@@ -119,8 +99,8 @@ def clear_rules(update: Update, context: CallbackContext):
     update.effective_message.reply_text("Successfully cleared rules!")
 
 
-def stats():
-    return f"• {sql.num_chats()} groups have rules."
+def __stats__():
+    return f"• {sql.num_chats()} chats have rules set."
 
 
 def __import_data__(chat_id, data):
@@ -129,27 +109,17 @@ def __import_data__(chat_id, data):
     sql.set_rules(chat_id, rules)
 
 
-def migrate(old_chat_id, new_chat_id):
+def __migrate__(old_chat_id, new_chat_id):
     sql.migrate_chat(old_chat_id, new_chat_id)
 
 
 def __chat_settings__(chat_id, user_id):
-    return f"This chat has had it's rules set: {bool(sql.get_rules(chat_id))}"
+    return f"This chat has had it's rules set: `{bool(sql.get_rules(chat_id))}`"
 
 
-help = """
- ‣ /rules*:* get the rules for this chat.
- ‣ /rules here*:* get the rules for this chat but send it in the chat.
-*Admins only:*
- ‣ /setrules <your rules here>*:* set the rules for this chat.
- ‣ /clearrules*:* clear the rules for this chat.
-"""
-
-__mod_name__ = "Rᴜʟᴇs"
-
-GET_RULES_HANDLER = CommandHandler("rules", get_rules)
-SET_RULES_HANDLER = CommandHandler("setrules", set_rules)
-RESET_RULES_HANDLER = CommandHandler("clearrules", clear_rules)
+GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
+SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
+RESET_RULES_HANDLER = CommandHandler("clearrules", clear_rules, filters=Filters.group)
 
 dispatcher.add_handler(GET_RULES_HANDLER)
 dispatcher.add_handler(SET_RULES_HANDLER)
